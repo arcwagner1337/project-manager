e820454d2e95b0ee546113ae1da0aa30ff5f433e
arcwagner1337 <wellernam1788@gmail.com>
2026-07-26 22:07:39 +0700

    rename and nginx fix

---
+++ server/Migrations/20260726145548_InitialCreateAfterRename.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace test_task.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateAfterRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
